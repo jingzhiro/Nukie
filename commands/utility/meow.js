2ca5595234645b5b002f0238e0a087004d9a799a
Zhi Zhao <cr3wwe@gmail.com>
2024-12-18 04:59:52 +1100

    More set-up, refactored code

---
+++ commands/utility/meow.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('meow')
+        .setDescription('Replies with Meow!'),
+    async execute(interaction) {
+        await interaction.reply('Meow!');
+    }
+};
